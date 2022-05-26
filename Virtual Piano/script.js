95d0bdfa5cc03c838cc3684e7d82f95ee937a908
Anja Labak <anja.labak12345@gmail.com>
2022-05-26 20:21:04 +0200

    Refaktoriranje i dodavanje datoteke Typesrcipt datoteke

--- Virtual Piano/script.js
+++ Virtual Piano/script.js
@@ -1,16 +1,27 @@
 document.addEventListener("keydown", event => {
     let whiteKeys = [   "KeyA",
-        "KeyS",
-        "KeyD",
-        "KeyF",
-        "KeyG",
-        "KeyH",
-        "KeyJ"];
+                        "KeyS",
+                        "KeyD",
+                        "KeyF",
+                        "KeyG",
+                        "KeyH",
+                        "KeyJ"];
 
     let blackKeys = [   "KeyW",
-        "KeyE",
-        "KeyT",
-        "KeyY",
-        "KeyU"];
-    
+                        "KeyE",
+                        "KeyT",
+                        "KeyY",
+                        "KeyU"];
+
+    if (whiteKeys.includes(event.code)) {
+        let audio = new Audio("white_keys/" + event.key.toUpperCase() + ".mp3");
+        audio.play();
+        console.log(`The '${event.key}' key is pressed.`)
+    } else if (blackKeys.includes(event.code)) {
+        let audio = new Audio("black_keys/" + event.key.toUpperCase() + ".mp3");
+        audio.play();
+        console.log(`The '${event.key}' key is pressed.`)
+    } else {
+        console.warn(`Unexpected keypress: '${event.code}'`)
+    }
 });
