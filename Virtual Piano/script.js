4fac290d065c46bf9cba52972b5d3a5938e4ffed
Anja Labak <anja.labak12345@gmail.com>
2022-08-27 14:36:02 +0200

    Dodavanje zvuka na crne tipke

--- Virtual Piano/script.js
+++ Virtual Piano/script.js
@@ -1,27 +1,31 @@
 document.addEventListener("keydown", event => {
-    let whiteKeys = [   "KeyA",
-        "KeyS",
-        "KeyD",
-        "KeyF",
-        "KeyG",
-        "KeyH",
-        "KeyJ"];
+    let whiteKeys = [   "A",
+        "S",
+        "D",
+        "F",
+        "G",
+        "H",
+        "J"];
 
-    let blackKeys = [   "KeyW",
-        "KeyE",
-        "KeyT",
-        "KeyY",
-        "KeyU"];
+    let blackKeys = [   "W",
+        "E",
+        "T",
+        "Z",
+        "U"];
 
     if (whiteKeys.indexOf(event.code)) {
         let audio = new Audio("white_keys/" + event.key.toUpperCase() + ".mp3");
         audio.play();
-        console.log(`The '${event.key}' key is pressed.`)
-    } else if (blackKeys.indexOf(event.code)) {
+        console.log("The '${event.key}' key is pressed.")
+    } else {
+        console.warn("Unexpected keypress: '${event.code}'")
+    }
+
+    if (blackKeys.indexOf(event.code)) {
         let audio = new Audio("black_keys/" + event.key.toUpperCase() + ".mp3");
         audio.play();
-        console.log(`The '${event.key}' key is pressed.`)
+         console.log("The '${event.key}' key is pressed.")
     } else {
-        console.warn(`Unexpected keypress: '${event.code}'`)
+         console.warn("UnexpYected keypress: '${event.code}'")
     }
 });
