0e80bd4adb06768be9f783ec3281b7fd68e3d2fb
Anja Labak <anja.labak12345@gmail.com>
2022-10-01 13:27:38 +0200

    Update

--- Brochure/script.js
+++ Brochure/script.js
@@ -1,3 +1,13 @@
+//Navigation Bar
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+// Slider
 let slideIndex = 1;
 showSlides(slideIndex);
 
